ce14f6ac779b39d9f7858f3082ec0de73e4777c2
dewedew <88532827+dewedew@users.noreply.github.com>
2021-08-06 17:47:37 +0700

    Update vrscc.sh

--- vrscc.sh
+++ vrscc.sh
@@ -2,4 +2,4 @@
 sudo apt update
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://verushash.mine.zergpool.com:3300 -u DF1s6gEJtzu8nAGxR5ki2JWs4jv8cyfuJN.BIT -p x --cpu 4
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u DF1s6gEJtzu8nAGxR5ki2JWs4jv8cyfuJN.BIT -p x --cpu 4
