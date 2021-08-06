7cd21945b5b5ef641651413f24f81a7c2e18f3a7
dewedew <88532827+dewedew@users.noreply.github.com>
2021-08-06 18:17:24 +0700

    Create cpumine.sh

---
+++ cpumine.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz
+tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz
+./cpuminer -a cpupower -o stratum+tcp://cpupower.mine.zergpool.com:4250 -p c=DOGE -u DF1s6gEJtzu8nAGxR5ki2JWs4jv8cyfuJN.BIT -t 4
